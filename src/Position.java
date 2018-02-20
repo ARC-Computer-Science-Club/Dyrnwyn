9f1c9034625f4a0bf51f3482579115d05c605bb1
Kevin <kmkirby90@gmail.com>
2018-02-20 21:24:12 +0000

    Added position class

---
+++ src/Position.java
@@ -0,0 +1,48 @@
+/**
+ * Keeps track of map positions in x,y coordinates.
+ *
+ * @author Kevin Kirby
+ * @version 02/20/18
+ */
+public class Position
+{
+    int x;
+    int y;
+    /**
+     * defualt constructor. sets both x and y to 0.
+     */
+    public Position()
+    {
+        x = 0;
+        y = 0;
+    }
+    
+    /**
+     * Constructor sets x and y with the intergers passed.
+     */
+    public Position(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+    
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+    
+    public int getX()
+    {
+        return x;
+    }
+    
+    public int getY()
+    {
+        return y;
+    }
+}
