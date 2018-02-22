62df0db9eaa2219016cebee869296a311e1ada37
Kevin <kmkirby90@gmail.com>
2018-02-22 22:01:13 +0000

    Fixed beaver constructor

--- src/Beaver.java
+++ src/Beaver.java
@@ -1,36 +1,38 @@
+import javax.swing.ImageIcon;
+
+/**
+ * Beaver character class
+ *
+ * @author Devin Scholl and Kevin Kirby
+ * @version 02/22/18
+ */
 public class Beaver extends Character
 {
     private int wood;
 
-
-    public Beaver()
+    public Beaver(ImageIcon icon, Position position, Movement movement)
     {
+        super(icon, position, movement);
         wood = 0;
     }
-
 
     public int getWood()
     {
         return wood;
     }
 
-
     public void setWood(int quantity)
     {
         wood = quantity;
     }
-
 
     public void addWood(int quantity)
     {
         wood += quantity;
     }
 
-
     public void removeWood(int quantity)
     {
         wood -= quantity;
     }
-
-
 }
