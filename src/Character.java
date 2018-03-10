a2cc7910763bd50f887a200f68c57db571243a64
Devin Scholl <DevinScholl1@gmail.com>
2018-03-10 00:10:24 -0800

    Changed indents from 2 spaces to 4 spaces. Added a getFacing() method.

--- src/Character.java
+++ src/Character.java
@@ -3,116 +3,122 @@ import javax.swing.ImageIcon;
 /**
  * Abstract class Character - Parent class for all characters.
  *
- * @author Devin Scholl, Eric Lum and Kevin Kirby 
+ * @author Devin Scholl, Eric Lum and Kevin Kirby
  * @version 02/22/18
  */
 public abstract class Character
 {
     // Attributes
-  private ImageIcon sprite;
-  private Position position;
-  private Movement movement;
-  private Direction direction;
-  
-  public enum Direction{
-    NORTH, EAST, SOUTH, WEST
-  }
-  
+    private ImageIcon sprite;
+    private Position position;
+    private Movement movement;
+    private Direction direction;
+
+    public enum Direction
+    {
+        NORTH, EAST, SOUTH, WEST
+    }
+
   // Constructor
-  
-  public Character(ImageIcon icon, Position position, Movement movement)
-  {
-    sprite = icon;
-    this.position = position;
-    this.movement = movement;
-  }
-  
-   // Methods
-  public Position getPosition()
-  {
-    return position;
-  }
 
-  public void setPosition(int x,int y)
-  {
-    position.setX(x);
-    position.setY(y);
-  }
+    public Character(ImageIcon icon, Position position, Movement movement)
+    {
+        sprite = icon;
+        this.position = position;
+        this.movement = movement;
+    }
 
-  public void setSprite(ImageIcon icon)
-  {
-    sprite = icon;
-  }
+    // Methods
+    public Position getPosition()
+    {
+        return position;
+    }
 
-  public ImageIcon getSprite()
-  {
-    return sprite;
-  }
-  
-  public void setMovement(Movement movement)
-  {
-      this.movement = movement;
-  }
-  
-  public void setFacing(Direction face)
-  {
-    direction = face;
-  }
-  
-  public void turnRight()
-  {
-    switch(direction)
+    public void setPosition(int x,int y)
     {
-      case NORTH:  direction = Direction.EAST;
-        break;
-      case EAST:  direction = Direction.SOUTH;
-        break;
-      case SOUTH:  direction = Direction.WEST;
-        break;
-      case WEST:  direction = Direction.NORTH;
-        break;
+        position.setX(x);
+        position.setY(y);
     }
-  }
-  
-  public void turnLeft()
-  {
-    switch(direction)
+
+    public void setSprite(ImageIcon icon)
     {
-      case NORTH:  direction = Direction.WEST;
-        break;
-      case EAST:  direction = Direction.NORTH;
-        break;
-      case SOUTH:  direction = Direction.EAST;
-        break;
-      case WEST:  direction = Direction.SOUTH;
-        break;
+        sprite = icon;
     }
-  }
-  
-  /**
-   * Map must check if move is valid before calling this function.
-   */
-  public void forward()
-  {
-    switch(direction)
+
+    public ImageIcon getSprite()
     {
-      case NORTH:
-        position.setY(position.getY() - 1);
-        break;
-      case EAST:
-        position.setX(position.getX() + 1);
-        break;
-      case SOUTH:
-        position.setY(position.getY() + 1);
-        break;
-      case WEST:
-        position.setX(position.getX() + 1);
-        break;
+        return sprite;
     }
-  }
-  
-  public Movement getMovement()
-  {
-    return movement;
-  }
+
+    public void setMovement(Movement movement)
+    {
+        this.movement = movement;
+    }
+
+    public void setFacing(Direction face)
+    {
+        direction = face;
+    }
+
+    public Direction getFacing()
+    {
+        return direction;
+    }
+
+    public void turnRight()
+    {
+        switch(direction)
+        {
+            case NORTH:  direction = Direction.EAST;
+            break;
+            case EAST:  direction = Direction.SOUTH;
+            break;
+            case SOUTH:  direction = Direction.WEST;
+            break;
+            case WEST:  direction = Direction.NORTH;
+            break;
+        }
+    }
+
+    public void turnLeft()
+    {
+        switch(direction)
+        {
+            case NORTH:  direction = Direction.WEST;
+            break;
+            case EAST:  direction = Direction.NORTH;
+            break;
+            case SOUTH:  direction = Direction.EAST;
+            break;
+            case WEST:  direction = Direction.SOUTH;
+            break;
+        }
+    }
+
+    /**
+    * Map must check if move is valid before calling this function.
+    */
+    public void forward()
+    {
+        switch(direction)
+        {
+            case NORTH:
+            position.setY(position.getY() - 1);
+            break;
+            case EAST:
+            position.setX(position.getX() + 1);
+            break;
+            case SOUTH:
+            position.setY(position.getY() + 1);
+            break;
+            case WEST:
+            position.setX(position.getX() + 1);
+            break;
+        }
+    }
+
+    public Movement getMovement()
+    {
+        return movement;
+    }
 }
